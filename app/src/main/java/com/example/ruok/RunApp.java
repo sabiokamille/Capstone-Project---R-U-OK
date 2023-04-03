e1f252371b4cd54df134b5e56ac3df1712b0f28c
sabiokamille <sabiokamille@gmail.com>
2023-04-03 16:16:29 -0400

    successfully create a user in temp firebase database and log into test app

--- app/src/main/java/com/example/ruok/RunApp.java
+++ app/src/main/java/com/example/ruok/RunApp.java
@@ -1,29 +1,52 @@
 package com.example.ruok;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ruok.ui.login.LoginActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //import com.example.caps2.R;
 
 
 public class RunApp extends AppCompatActivity {
-    EditText username;
-    EditText password;
-    Button loginButton;
-
+    FirebaseAuth auth;
+    TextView message;
+    Button logout;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
-        loginButton = findViewById(R.id.login);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+        auth = FirebaseAuth.getInstance();
+        logout = findViewById(R.id.logout_btn);
+        message = findViewById(R.id.welcome_message);
+        user = auth.getCurrentUser();
 
-
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            message.setText(user.getEmail());
+        }
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ruok/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/ruok/ui/login/LoginActivity.java
@@ -2,9 +2,11 @@ package com.example.ruok.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -23,14 +25,35 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ruok.R;
+import com.example.ruok.Registration;
+import com.example.ruok.RunApp;
 import com.example.ruok.ui.login.LoginViewModel;
 import com.example.ruok.ui.login.LoginViewModelFactory;
 import com.example.ruok.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
     private ActivityLoginBinding binding;
+
+    FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), RunApp.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +69,16 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
+        mAuth = FirebaseAuth.getInstance();
+        TextView gotToRegister = findViewById(R.id.register_new);
+        gotToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
             @Override
@@ -74,7 +107,25 @@ public class LoginActivity extends AppCompatActivity {
                     showLoginFailed(loginResult.getError());
                 }
                 if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
+                    mAuth.signInWithEmailAndPassword((usernameEditText.getText().toString()), passwordEditText.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    loadingProgressBar.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(), "Login Successful!",
+                                                Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), RunApp.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+//                    updateUiWithUser(loginResult.getSuccess());
                 }
                 setResult(Activity.RESULT_OK);
 
